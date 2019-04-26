01a547a838d14e3aec18b149aee2dd10013b02fa
gitshanks <nishankatwork@gmail.com>
2019-04-26 16:32:16 -0600

    Fixed camera

--- feedback_sensors/scripts/camera.py
+++ feedback_sensors/scripts/camera.py
@@ -2,24 +2,44 @@
 # license removed for brevity
 import rospy
 import time
+import cv2
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 def camera():
     rospy.init_node('camera', anonymous=True)
-
-    pub = rospy.Publisher('/sensors/camera', String, queue_size=10)
-    rate = rospy.Rate(0.1) # 10hz
-
-    time.sleep(2)
+    cap = cv2.VideoCapture(0)
+    bridge = CvBridge()
+    pub = rospy.Publisher('/sensors/camera', Image,queue_size=10)
+    # rate = rospy.Rate(0.1) # 10hz
+    
+    # time.sleep(2)
 
     while not rospy.is_shutdown():
         camera_output = "Camera time %s" % rospy.get_time()
         rospy.logwarn("CAMERA: Publishing camera data...")
-        pub.publish(camera_output)
-        rate.sleep()
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+    
+            # Display the resulting frame
+            # cv2.imshow('input-camera.py',frame)
+            
+            pub.publish(bridge.cv2_to_imgmsg(frame,"bgr8"))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+        
+        # rate.sleep()
 
 if __name__ == '__main__':
     try:
         camera()
     except rospy.ROSInterruptException:
         pass
+
+        
--- feedback_sensors/scripts/mocap.py
+++ feedback_sensors/scripts/mocap.py
@@ -3,20 +3,21 @@
 import rospy
 import time
 from std_msgs.msg import String
-
+from lfd.items import StaticObject
 def mocap():
     rospy.init_node('mocap', anonymous=True)
     pub = rospy.Publisher('/sensors/mocap', String, queue_size=10)
 
     rate = rospy.Rate(0.1) # 10hz
 
-    time.sleep(2)
+    left_hand = StaticObject(1, "left_hand", None, "world", "left_hand")
+
 
     while not rospy.is_shutdown():
         mocap_output = "MoCap time %s" % rospy.get_time()
         rospy.logwarn("MOCAP: Publishing mocap data...")
         pub.publish(mocap_output)
-        rate.sleep()
+        print(left_hand.get_state())
 
 if __name__ == '__main__':
     try:
