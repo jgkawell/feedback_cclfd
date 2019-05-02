9256fc3ae9bdf6a1463a4e5d01b9570388ecd3f9
Jack Kawell <jgkawell@outlook.com>
2019-05-02 15:15:45 -0600

    Fixed bugs in face classifier

--- feedback_classifiers/scripts/face.py
+++ feedback_classifiers/scripts/face.py
@@ -16,7 +16,7 @@ class face():
     def __init__(self):
         rospy.init_node('face', anonymous=True)
         #loading the model
-        self.json_file = open('fer.json', 'r')
+        self.json_file = open('/home/jgkawell/sawyer_ws/fer.json', 'r')
         self.loaded_model_json = self.json_file.read()
         self.json_file.close()
         self.loaded_model = model_from_json(self.loaded_model_json)
@@ -24,33 +24,28 @@ class face():
         self.counter =0
         self.emot ={0:0,1:0}
         # load weights into new model
-        self. loaded_model.load_weights("fer.h5")
+        self. loaded_model.load_weights("/home/jgkawell/sawyer_ws/fer.h5")
 
-        print("Loaded model from disk")
+        rospy.loginfo("Loaded model from disk")
 
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/sensors/camera", Image, self.callback)
         self.image_pub = rospy.Publisher('/classifiers/face', String, queue_size=10)
-        
-   
+
+        self.SHOW_FACE = rospy.get_param("SHOW_FACE")
 
     def callback(self, data):
         labels = ['Neutral','Negative']
         prev_emotion = 0
         self.counter+=1
-        print(self.counter)
-        
         
         try:
             full_size_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
-            rospy.logwarn("FACE: Image Recieved...")
         except CvBridgeError as e:
             print(e)
             
-        rospy.logwarn("FACE: Classifying emotion...")
-        print("Classifier: Image Loaded")
         gray = cv2.cvtColor(full_size_image, cv2.COLOR_BGR2GRAY)
-        face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        face = cv2.CascadeClassifier('/home/jgkawell/sawyer_ws/haarcascade_frontalface_default.xml')
         faces = face.detectMultiScale(gray, 1.3  , 10)
         for (x, y, w, h) in faces:
             roi_gray = gray[y:y + h, x:x + w]
@@ -60,23 +55,19 @@ class face():
             if(self.counter%1==0):
                 #predicting the emotion
                 yhat= self.loaded_model.predict(cropped_img)
-                print('yhat:',yhat[0])
                 index = np.argmax(yhat[0])
-                print('index',index)
-                print("Emotion: ",labels[index]) 
                 self.emot[index]+=1
                 if(self.counter%10==0):
                     emotion = max(self.emot, key=self.emot.get)
                     prev_emotion = emotion
                     self.emot={0:0,1:0}
-                    print("index 15: ",emotion)
                     cv2.putText(full_size_image, labels[emotion], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
                     self.image_pub.publish(labels[emotion])
-                    rospy.logwarn("FACE: I published %s",labels[emotion])
                 else:
                     cv2.putText(full_size_image, labels[prev_emotion], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
 
-        cv2.imshow('output-face',full_size_image)
+        if self.SHOW_FACE:
+            cv2.imshow('output  -face',full_size_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
     
