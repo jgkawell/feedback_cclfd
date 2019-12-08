2cb5de26c31e2e7853fb1d34431399b5406191a7
Jack Kawell <jgkawell@outlook.com>
2019-12-08 16:07:47 -0700

    Teleop now uses both motion and face

--- feedback_master/scripts/teleop.py
+++ feedback_master/scripts/teleop.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2.7
 # license removed for brevity
-import rospy
-from std_msgs.msg import Bool
 import os
+import rospy
 import numpy as np
+
+from std_msgs.msg import Bool
+
 
 """ This class allows for manual triggering of classification """
 
@@ -14,8 +16,10 @@ class teleop():
         rospy.init_node('teleop', anonymous=True)
 
         # initializing publisher
-        self.pub = rospy.Publisher(
+        self.pub_face = rospy.Publisher(
             '/classifiers/face', Bool, queue_size=10)
+        self.pub_motion = rospy.Publisher(
+            '/classifiers/motion', Bool, queue_size=10)
 
     def main(self):
 
@@ -24,7 +28,8 @@ class teleop():
                   " negative classification...")
             raw_input().upper()
             print("TELEOP: Trigging negative classification!")
-            self.pub.publish(False)
+            self.pub_face.publish(False)
+            self.pub_motion.publish(False)
 
 
 if __name__ == '__main__':
