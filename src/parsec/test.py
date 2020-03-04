4f01b622d7c7ed3f5ed96cafe2c1e8de6c40cce8
Jack Kawell <jgkawell@outlook.com>
2020-03-04 16:40:40 -0700

    Fixed manual mode

--- src/parsec/test.py
+++ src/parsec/test.py
@@ -16,18 +16,21 @@ parser = argparse.ArgumentParser(description='Run feedback tests')
 parser.add_argument('num_tests', type=int, default=10, help='Number of tests to run')
 parser.add_argument('mode', type=str, default='auto', help='Mode for tests (manual/auto)')
 parser.add_argument('data', type=str, default='basic', help='Data to run (basic/handover/pour/cleaning/generated)')
-
-# Config variables
-mode = "auto"
-faults = {}
+parser.add_argument('output', type=str, default='../../output', help='Output directory for results')
+parser.add_argument('config', type=str, default='../../config', help='Configuration directory')
 
 
-def nlp_test(processor):
+def nlp_test(input):
+    processor = input[0]
+    mode = input[1]
+    output_dir = input[2]
+    config_dir = input[3]
+    faults = input[4]
     questions_asked = {}
 
     # Create tree
     tree = Tree()
-    tree.build('../../config/constraints.yml', '../../config/parameters.yml')
+    tree.build(config_dir + '/constraints.yml', config_dir + '/parameters.yml')
 
     # Iterate through sentences
     for fault, data in faults.items():
@@ -65,7 +68,7 @@ def nlp_test(processor):
         question_nodes = sorted(question_nodes, key=lambda x: x.score, reverse=True)
 
         # Iterate over all questions to ask
-        corrected, current_count = iterate_over_nodes(tree, question_nodes, correct_params, current_count)
+        corrected, current_count = iterate_over_nodes(mode, tree, question_nodes, correct_params, current_count)
 
         # If the user responds no to everything
         if not corrected:
@@ -80,12 +83,17 @@ def nlp_test(processor):
     return questions_asked
 
 
-def tree_test(processor):
+def tree_test(input):
+    processor = input[0]
+    mode = input[1]
+    output_dir = input[2]
+    config_dir = input[3]
+    faults = input[4]
     questions_asked = {}
 
     # Create tree
     tree = Tree()
-    tree.build('../../config/constraints.yml', '../../config/parameters.yml')
+    tree.build(config_dir + '/constraints.yml', config_dir + '/parameters.yml')
 
     # Iterate through sentences
     for fault, data in faults.items():
@@ -116,7 +124,7 @@ def tree_test(processor):
         random.shuffle(question_nodes)
 
         # Iterate over all questions to ask
-        corrected, current_count = iterate_over_nodes(tree, question_nodes, correct_params, current_count)
+        corrected, current_count = iterate_over_nodes(mode, tree, question_nodes, correct_params, current_count)
 
         # If the user responds no to everything
         if not corrected:
@@ -131,12 +139,17 @@ def tree_test(processor):
     return questions_asked
 
 
-def tree_nlp_test(processor):
+def tree_nlp_test(input):
+    processor = input[0]
+    mode = input[1]
+    output_dir = input[2]
+    config_dir = input[3]
+    faults = input[4]
     questions_asked = {}
 
     # Create tree
     tree = Tree()
-    tree.build('../../config/constraints.yml', '../../config/parameters.yml')
+    tree.build(config_dir + '/constraints.yml', config_dir + '/parameters.yml')
 
     # Iterate through sentences
     for fault, data in faults.items():
@@ -165,7 +178,7 @@ def tree_nlp_test(processor):
         question_nodes = tree.get_questions()
 
         # Iterate over all questions to ask
-        corrected, current_count = iterate_over_nodes(tree, question_nodes, correct_params, current_count)
+        corrected, current_count = iterate_over_nodes(mode, tree, question_nodes, correct_params, current_count)
 
         # If the user responds no to everything
         if not corrected:
@@ -180,12 +193,12 @@ def tree_nlp_test(processor):
     return questions_asked
 
 
-def iterate_over_nodes(tree, question_nodes, correct_params, total_questions_asked):
+def iterate_over_nodes(mode, tree, question_nodes, correct_params, total_questions_asked):
     # Display question that will be asked
     corrected = False
     for node in question_nodes:
         # Recursively traverse questions in tree
-        result, total_questions_asked = node_handle(tree, node, correct_params, total_questions_asked)
+        result, total_questions_asked = node_handle(mode, tree, node, correct_params, total_questions_asked)
         # Alert user and finish if solution is found
         if result:
             if mode == "manual":
@@ -196,7 +209,7 @@ def iterate_over_nodes(tree, question_nodes, correct_params, total_questions_ask
     return corrected, total_questions_asked
 
 
-def node_handle(tree, node, correct_params, total_questions_asked):
+def node_handle(mode, tree, node, correct_params, total_questions_asked):
 
     if node.score != -1:
         # Generate and print question and ask for response
@@ -207,6 +220,7 @@ def node_handle(tree, node, correct_params, total_questions_asked):
             print("Question: {}".format(query))
             print("Confidence: {}".format(node.score))
 
+        # If there's only a single parameter force cast to tuple
         if type(node.params) == str:
             cur_params = (node.params, )
         else:
@@ -252,12 +266,12 @@ def node_handle(tree, node, correct_params, total_questions_asked):
                 # Iterate through children recursively if needed
                 children = tree.get_best_children(node.params)
                 for child in children:
-                    result, total_questions_asked = node_handle(tree, child, correct_params, total_questions_asked)
+                    result, total_questions_asked = node_handle(mode, tree, child, correct_params, total_questions_asked)
                     if result:
                         return True, total_questions_asked
         else:
-            tree.rescore(cur_params)
             # If user responds no
+            tree.rescore(cur_params)
             if mode == "manual":
                 print("Response: No")
             return False, total_questions_asked
@@ -266,72 +280,84 @@ def node_handle(tree, node, correct_params, total_questions_asked):
         return False, total_questions_asked
 
 
-def tester(num_tests, run_mode, data):
-    global mode
-    global faults
+def tester(num_tests, run_mode, data, output_dir, config_dir):
+    # Setup variables and data
     mode = run_mode
     faults = Data(data).faults
     num_explanations = len(faults.keys())
     print("---- STARTING ----")
 
-    nlp_results_file = open("../../data/nlp_feedback_results.csv", "w")
-    tree_results_file = open("../../data/tree_feedback_results.csv", "w")
-    tree_nlp_results_file = open("../../data/tree_nlp_feedback_results.csv", "w")
+    # Run tests using test helper
+    print("Running NLP tests...")
+    results_nlp = test_helper("nlp", num_tests, num_explanations, mode, output_dir, config_dir, faults)
+    print("Running Tree tests...")
+    results_tree = test_helper("tree", num_tests, num_explanations, mode, output_dir, config_dir, faults)
+    print("Running Tree-NLP tests...")
+    results_tree_nlp = test_helper("tree-nlp", num_tests, num_explanations, mode, output_dir, config_dir, faults)
 
-    if mode == "auto":
-        print("Running NLP tests...")
-        results_nlp = test_helper("nlp", num_tests, num_explanations)
-        print("Running Tree tests...")
-        results_tree = test_helper("tree", num_tests, num_explanations)
-        print("Running Tree-NLP tests...")
-        results_tree_nlp = test_helper("tree-nlp", num_tests, num_explanations)
+    # Open results files
+    nlp_results_file = open(output_dir + "/nlp_feedback_results.csv", "w")
+    tree_results_file = open(output_dir + "/tree_feedback_results.csv", "w")
+    tree_nlp_results_file = open(output_dir + "/tree_nlp_feedback_results.csv", "w")
 
-        # Write results to files
-        for row in results_nlp:
-            line = str(row)[1:-1]
-            nlp_results_file.write(line + "\n")
-        for row in results_tree:
-            line = str(row)[1:-1]
-            tree_results_file.write(line + "\n")
-        for row in results_tree_nlp:
-            line = str(row)[1:-1]
-            tree_nlp_results_file.write(line + "\n")
-    else:
-        print("Not implemented")
+    # Write results to files
+    for row in results_nlp:
+        line = str(row)[1:-1]
+        nlp_results_file.write(line + "\n")
+    for row in results_tree:
+        line = str(row)[1:-1]
+        tree_results_file.write(line + "\n")
+    for row in results_tree_nlp:
+        line = str(row)[1:-1]
+        tree_nlp_results_file.write(line + "\n")
 
+    # Close files
     nlp_results_file.close()
     tree_results_file.close()
     tree_nlp_results_file.close()
     print("---- FINISHED ----")
 
 
-def test_helper(case, num_tests, num_explanations):
+def test_helper(case, num_tests, num_explanations, mode, output_dir, config_dir, faults):
 
     # Create word processor
-    processor = ProcessInput('../../config/dictionaries.yml')
+    processor = ProcessInput(config_dir + "/dictionaries.yml")
     processor.buildDicts()
 
+    # Collect data from tests
     data = []
-    num_proc = 10
-    for i in range(0, num_tests):
-        p = Pool(processes=num_proc)
-        if case == "nlp":
-            new_data = p.map(nlp_test, [processor for j in range(num_proc)])
-        elif case == "tree":
-            new_data = p.map(tree_test, [processor for j in range(num_proc)])
-        elif case == "tree-nlp":
-            new_data = p.map(tree_nlp_test, [processor for j in range(num_proc)])
-        else:
-            print("Bad case: {}".format(case))
-        data.extend(new_data)
-        p.close()
-        print("Finished: {}/{}".format((i + 1) * num_proc, num_tests * num_proc))
+    if mode == "auto":
+        num_proc = 10
+        for i in range(0, num_tests):
+            p = Pool(processes=num_proc)
+            if case == "nlp":
+                new_data = p.map(nlp_test, [(processor, mode, output_dir, config_dir, faults) for j in range(num_proc)])
+            elif case == "tree":
+                new_data = p.map(tree_test, [(processor, mode, output_dir, config_dir, faults) for j in range(num_proc)])
+            elif case == "tree-nlp":
+                new_data = p.map(tree_nlp_test, [(processor, mode, output_dir, config_dir, faults) for j in range(num_proc)])
+            else:
+                print("Bad case: {}".format(case))
+            data.extend(new_data)
+            p.close()
+            print("Finished: {}/{}".format((i + 1) * num_proc, num_tests * num_proc))
+    else:
+        for i in range(0, num_tests):
+            if case == "nlp":
+                new_data = [nlp_test((processor, mode, output_dir, config_dir, faults))]
+            elif case == "tree":
+                new_data = [tree_test((processor, mode, output_dir, config_dir, faults))]
+            elif case == "tree-nlp":
+                new_data = [tree_nlp_test((processor, mode, output_dir, config_dir, faults))]
+            else:
+                print("Bad case: {}".format(case))
+            data.extend(new_data)
 
+    # Convert data into 2D array
     results = [[] for i in range(0, num_explanations)]
     for entry in data:
         for key, value in entry.items():
             results[key - 1].append(value)
-
     results = np.array(results).T.tolist()
 
     return results
@@ -342,4 +368,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Run all cases in a loop
-    tester(args.num_tests, args.mode, args.data)
+    tester(args.num_tests, args.mode, args.data, args.output, args.config)
